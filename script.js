81fc7bf9a57adda62e952c2c82eeda11b213d3fd
manuel14mds <manuel14mds@gmail.com>
2023-06-16 09:25:08 -0500

    basic scroll tracker animation js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 import "https://flackr.github.io/scroll-timeline/dist/scroll-timeline.js"
+
+const scrollTraceker = document.querySelector(".scroll-tracker")
+
+const scrollTrackingTimeline = new ScrollTimeline({
+    source: document.scrollingElement,
+    orientation: "block",
+    scrollOffsets:[CSS.percent(0), CSS.percent(98)]
+})
+
+scrollTraceker.animate(
+    {
+        transform: ["scaleX(0)", "scaleX(1)"]
+    },
+    {
+        duration: 1,
+        timeline: scrollTrackingTimeline,
+    }
+)
